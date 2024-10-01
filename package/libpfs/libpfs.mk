e0ed06dfcbef44a3e4a4cc877850c8a08dae3df6
Sid Agrawal <sid@sid-agrawal.ca>
2024-10-01 09:02:53 -0700

    add python3 as a dependency for pfs

--- package/libpfs/libpfs.mk
+++ package/libpfs/libpfs.mk
@@ -9,7 +9,7 @@ LIBPFS_SITE = $(call github,sid-agrawal,pfs,v$(LIBPFS_VERSION))
 LIBPFS_LICENSE = MIT
 LIBPFS_LICENSE_FILES = copyright
 LIBPFS_INSTALL_STAGING = YES
-LIBPFS_DEPENDENCIES = python-pybind11
+LIBPFS_DEPENDENCIES = python-pybind11 python3
 LIBPFS_CONF_OPTS = -Dpfs_BUILDROOT=ON
 
 $(eval $(cmake-package))
