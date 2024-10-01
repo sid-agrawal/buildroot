f8207c23e924fbc5f23537b42511bf3f03fcee73
Sid Agrawal <sid@sid-agrawal.ca>
2024-09-30 19:15:16 -0700

    add pfs compiling

--- package/libpfs/libpfs.mk
+++ package/libpfs/libpfs.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-LIBPFS_VERSION = 1.0.0
+LIBPFS_VERSION = 1.0.1
 LIBPFS_SITE = $(call github,sid-agrawal,pfs,v$(LIBPFS_VERSION))
 LIBPFS_LICENSE = MIT
 LIBPFS_LICENSE_FILES = copyright
 LIBPFS_INSTALL_STAGING = YES
 LIBPFS_DEPENDENCIES = python-pybind11
+LIBPFS_CONF_OPTS = -Dpfs_BUILDROOT=ON
 
 $(eval $(cmake-package))
